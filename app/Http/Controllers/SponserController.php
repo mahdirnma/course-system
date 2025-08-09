68be4c9f8e53b12661d5d1738a36512b6d735be0
mahdirnma <rahnamamahdi78@gmail.com>
2025-08-09 19:46:31 +0330

    create necessary files

---
+++ app/Http/Controllers/SponserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sponser;
+use App\Http\Requests\StoreSponserRequest;
+use App\Http\Requests\UpdateSponserRequest;
+
+class SponserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSponserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sponser $sponser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sponser $sponser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSponserRequest $request, Sponser $sponser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sponser $sponser)
+    {
+        //
+    }
+}
