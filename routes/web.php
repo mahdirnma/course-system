300e0fd42f0131a1b9cba0f1ffa95cbcc0887b50
mahdirnma <rahnamamahdi78@gmail.com>
2025-08-10 10:03:31 +0330

    create authenticate process

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[UserController::class,'index'])->name('dashboard');
+Route::middleware('guest')->group(function () {
+    Route::get('login',[AuthController::class,'loginForm'])->name('login.form');
+    Route::post('login',[AuthController::class,'login'])->name('login');
+    Route::get('register',[AuthController::class,'registerForm'])->name('register.form');
+    Route::post('register',[AuthController::class,'register'])->name('register');
+
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/',[UserController::class,'index'])->name('dashboard');
+    Route::post('logout',[AuthController::class,'logout'])->name('logout');
+});
